535ff47c8b0edfc2817bc02feb076c1cb70734ee
Peter Brunnader <p@brunnader.at>
2013-11-28 10:58:05 +0100

    Inheritance implementation almost finished. Call of the methods still missing in invoke function.

---
+++ assignment_1/source/inheritance.c
@@ -0,0 +1,103 @@
+/**
+ * This is the "assignment 0" for the course "Programming languages and Paradigms" 
+ * at the Stockholm University 
+ *
+ * assigment 1
+ * inheritance
+ *
+ * @author Peter Brunnader 
+ * @author Katrin Freihofner
+ * @version 1.0
+ **/
+
+#include <stdio.h>
+#include <string.h>
+
+#define METHOD void*
+
+
+typedef struct _Method
+{
+    void* method;
+    char* name;
+    int argnum;
+    void* next;
+} Method;
+
+typedef struct _Class
+{
+    void* super;
+    Method* firstMethod;
+} Class;
+
+typedef struct _Object {
+    Class* class;
+    char* msg;
+} Object;
+
+void printMsg(Object* self){
+    puts(self->msg);
+}
+
+void setMsg(Object* self, char* msg){
+    self->msg = msg;
+}
+void* invoke(void* receiver, char* methodname, int argnum, void* argvalue){
+    Object* obj = receiver;
+    Class* class = obj->class;
+    Method* method = class->firstMethod;
+    
+    while (method != NULL) {
+        if(method->name == methodname && method->argnum == argnum) {
+            printf("FOUND: ");
+            break;
+        }
+        
+        method = method->next;
+        if(method == NULL) {
+           if(class->super != NULL) {
+                class = class->super;
+                method = class->firstMethod;
+            }else{
+                printf("NOT FOUND: ");
+                break;
+            }
+        }
+    }
+
+    // void (*functionName)() = NULL;
+    // functionName = &method->method;
+    // functionName();
+
+    printf("%i, %s, %i, %s \n", &method->method, methodname, argnum, argvalue);
+}
+
+
+int main(int argc, char *argv[]) {
+    Method method2;
+    method2.method = (METHOD) setMsg;
+    method2.name = "setMsg";
+    method2.argnum = 1;
+    method2.next = NULL;
+
+    Method method;
+    method.method = (METHOD) printMsg;
+    method.name = "printMsg";
+    method.argnum = 0;
+    method.next = &method2;
+
+    Class class;
+    class.super = NULL;
+    class.firstMethod = &method;
+
+    Object obj;
+    obj.class = &class;
+
+    /* Now the important part */
+    invoke(&obj, "setMsg", 1, "Hello, world");
+    invoke(&obj, "printMsg", 0, NULL);
+    invoke(&obj, "setMsg", 1, "This is another message");
+    invoke(&obj, "printMsg", 0, NULL);
+    
+    return 0;
+}
