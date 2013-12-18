e64769833d1b26446a6c3a481ff8eea27a90fc12
Katrin Freihofner <S1210629007@students.fh-hagenberg.at>
2013-12-18 21:17:35 +0100

    read and write from console

--- assignment_2/source/Assignment02.pl
+++ assignment_2/source/Assignment02.pl
@@ -13,7 +13,6 @@ expression(Num, Expression).
 
 variable(X):-atom(X).
 
-
 expression(Z) --> num(Z).
 expression(Z) --> num(X), [+], expression(Y), {Z is X+Y}.
 expression(Z) --> num(X), [-], expression(Y), {Z is X-Y}.
@@ -21,33 +20,23 @@ num(D) --> [D], {number(D)}.
 
 expression_value(L, V) :- expression(V, L, []).
 
+get_input(X) :-
+repeat,
+write(`Enter term: `),
+read(STRING),
+expression_value(STRING, V),
+write(V).
 
 /***
-expression(X):-number(X).
-expression(X|Expression):-number(X),rest_expression(Expression).
-rest_expression(operator(Operator,X)|Expression):-operator(Operator),number(X),rest_expression(Expression).
-rest_expression(operator(Operator|X)):-operator(Operator), number(X).
+To run this:
 
-operator(plus):- +.
-operator(minus):- -.
-***/
+consult('/Applications/bin/Assignment02.pl).
 
-/***
-expression([Value]) --> value(Value).
-expression([Value|Expression]) --> value(Value), rest_expression(Expression).
-value(number(Number)) --> [Number], {number(Number)}.
-value(variable(Variable)) --> [Variable], {atom(Variable)}.
-rest_expression([operator(Operator,Value)|Expression]) --> operator(Operator), value(Value), rest_expression(Expression).
-rest_expression([operator(Operator,Value)]) --> operator(Operator), value(Value).
-operator(plus) --> [+].
-operator(minus) --> [-].
-***/
+...
 
-/***
-evaluate(A+B,CV,Vars):-eval_v(A,AV,Vars),eval_v(B,BV,Vars),CV is AV+BV.
-evaluate(A-B,CV,Vars):-eval_v(A,AV,Vars),eval_v(B,BV,Vars),CV is AV-BV.
-evaluate(A*B,CV,Vars):-eval_v(A,AV,Vars),eval_v(B,BV,Vars),CV is AV*BV.
-evaluate(Num,Num,Vars):-number(Num).
-evaluate(Var,Value,Vars):-atom(Var),member(Var/Value,Vars).
-
+yes
+| ?- get_input(X).
+Enter term: [3,+,2].
+5
+true ? yes
 ***/
