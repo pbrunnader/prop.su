1c56933a851003c82e03b4148e2857ec60e6b076
Peter Brunnader <p@brunnader.at>
2014-01-09 15:22:44 +0100

    A working version for all cases, unclear if 'Closeable' is correct implemneted.

--- assignment_3/source/SafeMacro.clj
+++ assignment_3/source/SafeMacro.clj
@@ -1,17 +1,33 @@
-(defmacro safe [& form] 
-  `(try
-    ~@form
+(import (java.io FileReader File))
+ 
+(defmacro safe [param & form] 
+  (if (vector? param)
+    `(try
+      (let [~(first param) ~(second param)] (def a# ~@form)
+      (if (instance? java.io.Closeable ~(first param)) 
+        (. ~(first param) close) 
+        (println "nothing.") 
+      ) a#)
     (catch Exception e# e#)
-    (finally (println "Done."))))
+    (finally (println "First case.")) 
+  )
+    
+  `(try
+    ~param
+  (catch Exception e# e#)
+  (finally (println "Second case.")))
+  )
+)
 
 
-
-(def v (safe (/ 1 0)))
+(def v (safe (/ 10 5)))
 (println v)
 
-(def v (safe (/ 10 2)))
+(def v (safe (/ 10 0)))
 (println v)
 
 (def v (safe [s (FileReader. (File. "file.txt"))] (. s read)))
 (println v)
 
+(def v (safe [s (FileReader. (File. "missing-file"))] (. s read)))
+(println v)
